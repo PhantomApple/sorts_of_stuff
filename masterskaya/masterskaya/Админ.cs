0e3780a8364913acd27dd18e9cba1f16ad339fc2
11Nikitos11 <156991863+11Nikitos11@users.noreply.github.com>
2024-05-29 19:18:26 +0300

    мастерская

---
+++ masterskaya/masterskaya/Админ.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace masterskaya
+{
+    public partial class Админ : Form
+    {
+        public Админ()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Авто авто = new Авто();
+            авто.Show();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Механики механики = new Механики();
+            механики.Show();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Заказы заказы = new Заказы();
+            заказы.Show();
+        }
+
+        private void Админ_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
